5bfa2c8d4260f4f3c1298a75c0ec9e737503be15
varholak-peter <peter.varholak@gmail.com>
2020-04-25 03:29:39 +0200

    Updates

---
+++ precache-manifest.11d00abeddb0914fd39209ffe2025e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f349e88c7cf864f441fb04fd38d635",
+    "url": "/project-estimator/index.html"
+  },
+  {
+    "revision": "18f4675fd7d46e4bff1d",
+    "url": "/project-estimator/static/css/main.861201f5.chunk.css"
+  },
+  {
+    "revision": "3a9be77947485c3e24ec",
+    "url": "/project-estimator/static/js/2.f6c8c520.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/project-estimator/static/js/2.f6c8c520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f4675fd7d46e4bff1d",
+    "url": "/project-estimator/static/js/main.8cee3fbe.chunk.js"
+  },
+  {
+    "revision": "4409030d73e99000a503",
+    "url": "/project-estimator/static/js/runtime-main.21b1b49e.js"
+  }
+]);
